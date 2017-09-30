e466e3d96cc06f90f0228f9fa05108ec7495d919
David Vallner <david@vallner.net>
2017-10-01 01:40:29 +0200

    Add Autofac module for the model project

---
+++ Massive.Interview.Model.Module/ModelModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massive.Interview.Model.Module
+{
+    public class ModelModule : Autofac.Module
+    {
+        public string ConnectionString { get; set; }
+
+        private GraphModel NewModel()
+        {
+            if (string.IsNullOrEmpty(ConnectionString))
+            {
+                return new GraphModel();
+            } else
+            {
+                return new GraphModel(ConnectionString);
+            }
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(ctx => NewModel()).AsSelf();
+        }
+    }
