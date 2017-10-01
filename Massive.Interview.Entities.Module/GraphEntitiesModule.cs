8c70ea23af7b582c229c8fe2d5e6b28c0420e789
David Vallner <millimoose@users.noreply.github.com>
2017-10-01 04:42:09 +0200

    add lifetime scope suppot to entities module

--- Massive.Interview.Entities.Module/GraphEntitiesModule.cs
+++ Massive.Interview.Entities.Module/GraphEntitiesModule.cs
@@ -1,31 +1,37 @@
 ﻿using Autofac;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Massive.Interview.Entities.Module
 {
+    /// <summary>
+    /// An Autofac module that registers the EF <c>DbContext</c> for the graph model.
+    /// </summary>
     public class GraphEntitiesModule : Autofac.Module
     {
+        /// <summary>
+        /// The connection string to initialize the <c>DbContext</c>.
+        /// </summary>
+        /// If this property is not set, uses the default connection string.
+        [NotNull]
         public string ConnectionString { get; set; }
 
         private GraphEntities NewDbContext()
         {
-            if (string.IsNullOrEmpty(ConnectionString))
-            {
-                return new GraphEntities();
-            }
-            else
-            {
-                return new GraphEntities(ConnectionString);
-            }
+            return string.IsNullOrEmpty(ConnectionString) 
+                ? new GraphEntities()
+                : new GraphEntities(ConnectionString);
         }
 
         protected override void Load(ContainerBuilder builder)
         {
-            builder.Register(ctx => NewDbContext()).AsSelf();
+            builder.Register(ctx => NewDbContext())
+                .InstancePerMatchingLifetimeScope(nameof(DbContext))
+                .AsSelf();
         }
     }
 }
