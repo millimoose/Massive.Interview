056c7a7f18000cca170a7fb4aaf576e4e399d620
David Vallner <millimoose@users.noreply.github.com>
2017-10-01 17:15:23 +0200

    implement loading a directory of node files

---
+++ Massive.Interview.LoaderApp/NodeDocumentDirectoryBatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Massive.Interview.LoaderApp
+{
+    /// <summary>
+    /// A document batch that loads all files in a directory.
+    /// </summary>
+    class NodeDocumentDirectoryBatch : INodeDocumentBatch
+    {
+        private DirectoryInfo _directory;
+        private INodeDocumentReader _reader;
+        private string _searchPattern;
+
+        public NodeDocumentDirectoryBatch(DirectoryInfo directory, INodeDocumentReader reader, string searchPattern)
+        {
+            _directory = directory ?? throw new ArgumentNullException(nameof(directory));
+            _reader = reader ?? throw new ArgumentNullException(nameof(reader));
+            _searchPattern = searchPattern;
+        }
+
+        public Task<NodeInput[]> LoadDocumentsAsync()
+        {
+            var files = _searchPattern == null 
+                ? _directory.EnumerateFiles()
+                : _directory.EnumerateFiles(_searchPattern);
+
+            return Task.WhenAll(from file in files select LoadDocumentAsync(file));
+
+        }
+
+        /// <summary>
+        /// Load a single file
+        /// </summary>
+        private Task<NodeInput> LoadDocumentAsync(FileInfo file)
+        {
+            using (var stream = file.OpenRead())
+            {
+                return _reader.ParseNodeInputAsync(stream);
+            }
+        }
+    }
+}
