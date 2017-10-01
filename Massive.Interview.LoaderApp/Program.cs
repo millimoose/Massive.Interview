6e3aee038db22be4608383c1935bf632634a5a9d
David Vallner <millimoose@users.noreply.github.com>
2017-10-02 01:21:25 +0200

    finish converting to Core

--- Massive.Interview.LoaderApp/Program.cs
+++ Massive.Interview.LoaderApp/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Massive.Interview.LoaderApp.Tests")]
 
 namespace Massive.Interview.LoaderApp
 {
