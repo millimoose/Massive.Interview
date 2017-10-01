f9ae5faafa84bb49fd75d5486ec80cab64190c38
David Vallner <millimoose@users.noreply.github.com>
2017-10-01 23:25:48 +0200

    switch over to .NET Core and EF Core

--- Massive.Interview.SmokeTestApp/SmokeTestApp.cs
+++ Massive.Interview.SmokeTestApp/SmokeTestApp.cs
@@ -2,35 +2,42 @@
 using Massive.Interview.Entities;
 using Massive.Interview.Entities.Module;
 using System;
-using System.Data.Entity;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
 
-namespace Massive.Interview.SmokeApp
+namespace Massive.Interview.SmokeTestApp
 {
     /// <summary>
     /// Basic sanity test / scratchpad for the model
     /// </summary>
     class Program
     {
+        class SmokeTestSettings
+        {
+
+            public GraphEntitiesSettings Entities { get; set; }
+        }
+
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("SmokeTestApp.json", true)
+                .AddCommandLine(args)
+                .Build();
+
+            var settings = new SmokeTestSettings();
+            config.Bind(settings);
+
             var builder = new ContainerBuilder();
-            builder.RegisterModule(new GraphEntitiesModule());
+            builder.RegisterModule(new GraphEntitiesModule(settings.Entities));
             builder.RegisterType<Program>().AsSelf();
             using (var container = builder.Build())
             {
                 WithScopedProgram(container, _ => _.CreateSomeNodes());
-                WithScopedProgram(container, _ => _.TestLeftToRight());
-                WithScopedProgram(container, _ => _.TestRightToLeft());
             }
-        }
-
-        private void TestRightToLeft()
-        {
-        }
-
-        private void TestLeftToRight()
-        {
         }
 
         /// <summary>
@@ -65,18 +72,26 @@ namespace Massive.Interview.SmokeApp
 
         private void CreateSomeNodes()
         {
-            var left = Context.Nodes.Create();
-            left.NodeId = 1;
-            left.Label = "Node1";
-            left = Context.Nodes.Add(left);
+            Context.Database.EnsureCreated();
+            var left = new Node
+            {
+                NodeId = 1,
+                Label = "Node1"
+            };
+            Context.Nodes.Add(left);
 
-            var right = Context.Nodes.Create();
-            right.NodeId = 2;
-            right.Label = "Node2";
-            right = Context.Nodes.Add(right);
+            var right = new Node
+            {
+                NodeId = 2,
+                Label = "Node2"
+            };
 
-            left.RightAdjacentNodes.Add(right);
-            right.LeftAdjacentNodes.Add(left);
+            var link = new AdjacentNode
+            {
+                LeftNode = left,
+                RightNode = right
+            };
+            Context.Add(link);
 
             Context.SaveChanges();
         }
