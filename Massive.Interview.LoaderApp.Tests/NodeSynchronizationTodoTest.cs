4ab9e5bb0d4b677e770cccad8b72891548fa820f
David Vallner <david@vallner.net>
2017-10-01 18:39:47 +0000

    node synchronization todo list class

---
+++ Massive.Interview.LoaderApp.Tests/NodeSynchronizationTodoTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Massive.Interview.LoaderApp.Tests
+{
+    [TestClass]
+    public class NodeSynchronizationTodoTest
+    {
+        [TestMethod]
+        public void Create_todo()
+        {
+            var previousIds = new long[] { 1, 2, 3, 4 };
+            var newIds = new long[] { 3, 4, 5, 6 };
+            var newNodes = from id in newIds select new NodeInput { Id = id };
+        
+            var todo = new NodeSynchronizationTodo(previousIds, newNodes);
+
+            CollectionAssert.AreEquivalent(
+                new long[] { 1, 2 },
+                todo.NodeIdsToRemove.ToArray());
+
+            CollectionAssert.AreEquivalent(
+                new long[] { 3, 4 },
+                (from node in todo.NodesToUpdate select node.Id).ToArray());
+
+            CollectionAssert.AreEquivalent(
+                new long[] { 5, 6 },
+                (from node in todo.NodesToAdd select node.Id).ToArray());
+        }
+    }
+}
---
+++ Massive.Interview.LoaderApp/NodeSynchronizationTodo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massive.Interview.LoaderApp
+{
+    /// <summary>
+    /// The set of operations that will synchronize the database with loaded input data.
+    /// </summary>
+    class NodeSynchronizationTodo
+    {
+        /// <summary>
+        /// New nodes that will be added to the database.
+        /// </summary>
+        public IReadOnlyCollection<NodeInput> NodesToAdd { get; }
+        /// <summary>
+        /// Nodes in the database that need to be updated.
+        /// </summary>
+        public IReadOnlyCollection<NodeInput> NodesToUpdate { get; }
+
+        /// <summary>
+        /// Nodes that will be removed from the database.
+        /// </summary>
+        public IReadOnlyCollection<long> NodeIdsToRemove { get; }
+
+        /// <summary>
+        /// Create a set of synmchronisation operations based on node IDs 
+        /// currently in the database, and loaded nodes.
+        /// </summary>
+        /// <param name="oldNodeIds">previous node IDs</param>
+        /// <param name="newNodes">new nodes</param>
+        public NodeSynchronizationTodo(IEnumerable<long> oldNodeIds, IEnumerable<NodeInput> newNodes)
+        {
+            var toAdd = new List<NodeInput>();
+            var toUpdate = new List<NodeInput>();
+
+            var oldSet = new HashSet<long>(oldNodeIds);
+            foreach (var node in newNodes)
+            {
+                if (oldSet.Remove(node.Id))
+                {
+                    // 
+                    toUpdate.Add(node);
+                } else
+                {
+                    toAdd.Add(node);
+                }
+            }
+
+            NodesToAdd = toAdd.AsReadOnly();
+            NodesToUpdate = toUpdate.AsReadOnly();
+            NodeIdsToRemove = oldSet.ToList().AsReadOnly();
+        }
+    }
+
+}
