207c20e6d71eed658912d45a50ca1e72d6bdc73a
David Vallner <millimoose@users.noreply.github.com>
2017-10-02 09:15:30 +0200

    add a blank service for retrieving the whole graph

--- Massive.Interview.Service/GraphServiceModule.cs
+++ Massive.Interview.Service/GraphServiceModule.cs
@@ -8,6 +8,7 @@ namespace Massive.Interview.Service
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<LoaderService>().AsSelf();
+            builder.RegisterType<GraphService>().AsSelf();
             builder.RegisterType<NodeSynchronizer>().AsImplementedInterfaces();
         }
     }
