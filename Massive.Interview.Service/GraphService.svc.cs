02738b8876c448848334f9d8a1c917d9782cca0a
David Vallner <david@vallner.net>
2017-10-03 00:09:41 +0200

    add graph service endpoint

--- Massive.Interview.Service/GraphService.svc.cs
+++ Massive.Interview.Service/GraphService.svc.cs
@@ -4,15 +4,34 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Threading.Tasks;
+using Massive.Interview.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Massive.Interview.Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GraphService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select GraphService.svc or GraphService.svc.cs at the Solution Explorer and start debugging.
-    public class GraphService : IGraphService
+    class GraphService : IGraphService
     {
-        public void DoWork()
+        GraphEntities _db;
+
+        public GraphService(GraphEntities db)
         {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public async Task<GraphData> GetGraphAsync()
+        {
+            var result = new GraphData();
+            result.Nodes = await (from dbNode in _db.Nodes
+                                  select new NodeData { Id = dbNode.NodeId.Value, Label = dbNode.Label })
+                                  .ToArrayAsync();
+            result.AdjacentNodes = await (from dbAdjacent in _db.AdjacentNodes
+                                          select new AdjacentNodeData {
+                                              LeftId = dbAdjacent.LeftNodeId.Value,
+                                              RightId = dbAdjacent.RightNodeId.Value
+                                          }).ToArrayAsync();
+
+            return result;
         }
     }
 }
