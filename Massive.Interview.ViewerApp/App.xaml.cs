e3fec3be62070747dd129ef7c51570d11b62a49f
David Vallner <david@vallner.net>
2017-10-03 06:49:34 +0200

    migrate to 4.7 and add a viewer app project

---
+++ Massive.Interview.ViewerApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Autofac;
+using Caliburn.Micro.Autofac;
+
+namespace Massive.Interview.ViewerApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App() => InitializeComponent();
+    }
+
+    public class AppBootstrapper : AutofacBootstrapper<ShellViewModel>
+    {
+        public AppBootstrapper() => Initialize();
+        protected override void OnStartup(object sender, StartupEventArgs e) => DisplayRootViewFor<ShellViewModel>();
+        protected override void ConfigureContainer(ContainerBuilder builder) {
+            builder.RegisterModule<ViewerModule>();
+        }
+
+    }
+}
---
+++ Massive.Interview.ViewerApp/ShellViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Massive.Interview.ViewerApp
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+        string _hello = "Hello";
+
+        public string Hello {
+            get => _hello;
+            set {
+                _hello = value;
+                NotifyOfPropertyChange(() => Hello);
+            }
+        }
+    }
+}
