58eecad36e013f1d15b32b923ebd69138e83d602
David Vallner <millimoose@users.noreply.github.com>
2017-10-03 06:49:34 +0200

    migrate to 4.7 and add a viewer app project

---
+++ Massive.Interview.ViewerApp/ViewerModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Massive.Interview.ViewerApp.Remote;
+
+namespace Massive.Interview.ViewerApp
+{
+    class ViewerModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<GraphServiceClient>().AsImplementedInterfaces();
+
+            builder.RegisterType<App>();
+            builder.RegisterType<ShellViewModel>();
+        }
+    }
+}
